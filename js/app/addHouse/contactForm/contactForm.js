857aa0bfd3ba5040f057c0d06e32bb3c03ad6768
Diego Mendoza <dmendoza@inflexionsoftware.com>
2017-07-23 22:19:50 -0500

    Pruebas menores para utilizar localstores, para persistir información e los diferentes pasos del Wizard

--- js/app/addHouse/contactForm/contactForm.js
+++ js/app/addHouse/contactForm/contactForm.js
@@ -3,15 +3,36 @@
 
 	angular.module('chaiApp.contactForm').controller('ContactFormCtrl', ContactFormCtrl);
 
+	ContactFormCtrl.$inject = ['$window'];
+
 	/**@ngInject*/
-	function ContactFormCtrl(){
+	function ContactFormCtrl($window){
 		var homeCtrl = this;
+
+		//attributes
+		var counter = 0;
+		//events
+		homeCtrl.clickNext = clickNext;
 
 		//Initialize controller
 		activate();
 
 		function activate(){
 			console.log('Activated ContactFormCtrl');	
+		}
+
+		function clickNext($event){
+			$event.preventDefault();
+			console.log('Next');
+
+			if(counter === 0){
+				$window.localStorage.setItem('test','super valor de prueba');
+			}else{
+				console.log($window.localStorage.getItem('test'));
+				$window.localStorage.setItem('test','super valor de prueba 2');
+			}
+
+			counter++;
 		}
 	}
 })();
