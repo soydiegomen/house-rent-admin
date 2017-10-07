3d2d4cb2c795665f2c6c6d565aae8139acbfc65f
Diego Mendoza <dmendoza@inflexionsoftware.com>
2017-10-06 22:58:57 -0500

    Agregue un parche para limpiar la información del local storage, cuando entramos a la pagina del listado de casas. Esto evita que se muestre información de otras sesiones en el formulario.

--- js/app/listHouses/listHouses.js
+++ js/app/listHouses/listHouses.js
@@ -3,10 +3,10 @@
 
 	angular.module('chaiApp.listHouses').controller('ListHousesCtrl', ListHousesCtrl);
 
-	ListHousesCtrl.$inject = ['dataservice'];
+	ListHousesCtrl.$inject = ['dataservice', 'houseStorageService'];
 
 	/**@ngInject*/
-	function ListHousesCtrl(dataservice){
+	function ListHousesCtrl(dataservice, houseStorageService){
 		var listCtrl = this;
 
 		//Properties
@@ -18,6 +18,8 @@
 		function activate(){
 			console.log('Activated ListHousesCtrl');	
 			fillHouseList();
+			//Clear local storage data. Avoid when navigate to the house form, show information loaded in other time
+			houseStorageService.clear();
 		}
 
 		function fillHouseList(){
